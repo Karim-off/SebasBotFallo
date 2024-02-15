41e04d811720b010b70c8b5066d805ce6e193e52
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-02-15 17:30:06 -0400

    Actualizar gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -9,7 +9,7 @@ let oi = `*𝙈𝙚𝙣𝙨𝙖𝙟𝙚:* ${pesan}`
 let teks = `╭━┄〔 *${wm}* 〕┄⊱\n│ 𝘼𝙘𝙩𝙞𝙫𝙚𝙣𝙨𝙚 𝙥𝙡𝙖𝙣𝙩𝙖𝙨 ⚡*\n│\n│❏ ${oi}\n│\n│❏ *𝙻𝙾𝚂 𝙸𝙽𝚅𝙾𝙲𝙾 𝙱𝙾𝚃𝚂:*\n`
 for (let mem of participants) {
 teks += `│➯⚡ @${mem.id.split('@')[0]}\n`}
-teks += `╰── @𝘿𝙮𝙡𝙖𝙣𝘽𝙤𝙩-𝙈𝘿⚡ \n\n`
+teks += `╰── @𝙎𝙚𝙗𝙖𝙨𝘽𝙤𝙩⚡ \n\n`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 conn.sendFile(m.chat, vn, 'Invocar.mp3', null, m, true, { type: 'audioMessage', ptt: true, sendEphemeral: true })
 }
