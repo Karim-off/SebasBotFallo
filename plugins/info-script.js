b7dca5e4e76b907dbd9656f47c4413c266159a09
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-02-16 15:09:49 -0400

    Actualizar info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -1,14 +1,14 @@
 import moment from 'moment-timezone';
 import fetch from 'node-fetch';
 const handler = async (m, { conn, args }) => {
-   const res = await fetch('https://api.github.com/repos/JOSTINofc/Goku-Bot-Lite');
+   const res = await fetch('https://www.instagram.com/usxr_angelito/');
    const json = await res.json();
    let txt = `           *乂  B O T  -  S C R I P T  乂*\n\n`;
-      txt += `◦  *Nombre:* ${json?.name || 'GokuBotLite-MD'}\n`;
+      txt += `◦  *Nombre:* ${json?.name || '𝙎𝙀𝘽𝘼𝙎 𝘽𝙊𝙏'}\n`;
       txt += `◦  *Visitantes:* ${json?.watchers_count || '-'}\n`;
       txt += `◦  *Tamaño:* ${(json?.size / 1024).toFixed(2) || '-'} MB\n`;
       txt += `◦  *Actualización:* ${moment(json?.updated_at).format('DD/MM/YY - HH:mm:ss') || '-'}\n`;
-      txt += `◦  *Url:* ${json?.html_url || global.md}\n\n`;
+      txt += `◦  *Url:* https://www.instagram.com/usxr_angelito/\n\n`;
       txt += `	 ${json?.forks_count || '-'} Forks · ${json?.stargazers_count || '-'} Stars · ${json?.open_issues_count || '-'} Issues`;
    await conn.sendMessage(m.chat, {text: txt.trim(), mentions: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm2, "containsAutoReply": true, "mediaType": 1, "thumbnail": imagen6, "mediaUrl": global.md, "sourceUrl": global.md}}}, {quoted: m});
 };
